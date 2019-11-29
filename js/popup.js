f761f617778918101ef1b3f57b0394ba7f4e952c
Dima Troshkin <villainrox@gmail.com>
2019-11-30 02:14:34 +0300

    Добавлен JS

---
+++ js/popup.js
@@ -0,0 +1,37 @@
+var open = document.querySelector(".button-address");
+var popup = document.querySelector(".modal-connection");
+var close = document.querySelector(".modal-close");
+var form = popup.querySelector(".connection-form");
+var nameFamily = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+
+open.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  nameFamily.focus();
+
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!nameFamily.value || !email.value || !text.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+    }
+  }
+});
